1f72d054bdddbfd7e2ccfac6aa6fee4a1e56a7ff
Lucky <you@example.com>
2018-02-20 23:22:20 +0530

    enhancement in MTW

---
+++ MTW.js
@@ -0,0 +1,245 @@
+             var text  = "";
+             var FR_LN = "";
+             var TO_LN = "";
+             var text  = "";
+             function getSelectValue_from_lang()
+        {
+            FR_LN = document.getElementById("from_lang").value;
+            console.log(FR_LN);
+        }
+        getSelectValue_from_lang();
+
+
+            function getSelectValue_to_lang()
+        {
+            TO_LN = document.getElementById("to_lang").value;
+            console.log(TO_LN);
+        }
+        getSelectValue_to_lang();
+         
+        function myText()
+       {
+           text = document.getElementById("userInput").value;
+       }
+
+            if(FR_LN == "Afrikaans"){ FR_LN = "af" };
+            if(FR_LN == "Albanian "){ FR_LN = "sq" };
+            if(FR_LN == "Amharic"){ FR_LN = "am" };
+            if(FR_LN == "Arabic"){ FR_LN = "ar" };
+            if(FR_LN == "Armenian"){ FR_LN = "hy" };
+            if(FR_LN == "Azeerbaijani"){ FR_LN = "az" };
+            if(FR_LN == "Basque"){ FR_LN = "eu" };
+            if(FR_LN == "Belarusian"){ FR_LN = "be" };
+            if(FR_LN == "Bengali"){ FR_LN = "bn" };
+            if(FR_LN == "Bosnian"){ FR_LN = "bs" };
+            if(FR_LN == "Bulgarian"){ FR_LN = "bg" };
+            if(FR_LN == "Catalan"){ FR_LN = "ca" };
+            if(FR_LN == "Cebuano"){ FR_LN = "ceb" };
+            if(FR_LN == "Chinese (Simplified)"){ FR_LN = "zh-CN" };
+            if(FR_LN == "Chinese (Traditional)"){ FR_LN = "zh-TW" };
+            if(FR_LN == "Corsican"){ FR_LN = "co" };
+            if(FR_LN == "Croatian"){ FR_LN = "hr" };
+            if(FR_LN == "Czech"){ FR_LN = "cs" };
+            if(FR_LN == "Danish"){ FR_LN = "da" };
+            if(FR_LN == "Dutch"){ FR_LN = "nl" };
+            if(FR_LN == "English"){ FR_LN = "en" };
+            if(FR_LN == "Esperanto"){ FR_LN = "eo" };
+            if(FR_LN == "Estonian"){ FR_LN = "et" };
+            if(FR_LN == "Finnish"){ FR_LN = "fi" };
+            if(FR_LN == "French"){ FR_LN = "fr" };
+            if(FR_LN == "Frisian"){ FR_LN = "fy" };
+            if(FR_LN == "Galician"){ FR_LN = "gl" };
+            if(FR_LN == "Georgian"){ FR_LN = "ka" };
+            if(FR_LN == "German"){ FR_LN = "de" };
+            if(FR_LN == "Greek"){ FR_LN = "el" };
+            if(FR_LN == "Gujarati"){ FR_LN = "gu" };
+            if(FR_LN == "Haitian Creole"){ FR_LN = "ht" };
+            if(FR_LN == "Hausa"){ FR_LN = "ha" };
+            if(FR_LN == "Hawaiian"){ FR_LN = "haw" };
+            if(FR_LN == "Hebrew"){ FR_LN = "iw" };
+            if(FR_LN == "Hindi"){ FR_LN = "hi" };
+            if(FR_LN == "Hmong"){ FR_LN = "hmn" };
+            if(FR_LN == "Hungarian"){ FR_LN = "hu" };
+            if(FR_LN == "Icelandic"){ FR_LN = "is" };
+            if(FR_LN == "Igbo"){ FR_LN = "ig" };
+            if(FR_LN == "Indonesian"){ FR_LN = "id" };
+            if(FR_LN == "Irish"){ FR_LN = "ga" };
+            if(FR_LN == "Italian"){ FR_LN = "it" };
+            if(FR_LN == "Japanese"){ FR_LN = "ja" };
+            if(FR_LN == "Javanese"){ FR_LN = "jw" };
+            if(FR_LN == "Kannada"){ FR_LN = "kn" };
+            if(FR_LN == "Kazakh"){ FR_LN = "kk" };
+            if(FR_LN == "Khmer"){ FR_LN = "km" };
+            if(FR_LN == "Korean"){ FR_LN = "ko" };
+            if(FR_LN == "Kurdish"){ FR_LN = "ku" };
+            if(FR_LN == "Kyrgyz"){ FR_LN = "ky" };
+            if(FR_LN == "Lao"){ FR_LN = "lo" };
+            if(FR_LN == "Latin"){ FR_LN = "la" };
+            if(FR_LN == "Latvian"){ FR_LN = "lv" };
+            if(FR_LN == "Lithuanian"){ FR_LN = "lt" };
+            if(FR_LN == "Luxembourgish"){ FR_LN = "lb" };
+            if(FR_LN == "Macedonian"){ FR_LN = "mk" };
+            if(FR_LN == "Malagasy"){ FR_LN = "mg" };
+            if(FR_LN == "Malay"){ FR_LN = "ms" };
+            if(FR_LN == "Malayalam"){ FR_LN = "ml" };
+            if(FR_LN == "Maltese"){ FR_LN = "mt" };
+            if(FR_LN == "Maori"){ FR_LN = "mi" };
+            if(FR_LN == "Marathi"){ FR_LN = "mr" };
+            if(FR_LN == "Mongolian"){ FR_LN = "mn" };
+            if(FR_LN == "Myanmar (Burmese)"){ FR_LN = "my" };
+            if(FR_LN == "Nepali"){ FR_LN = "ne" };
+            if(FR_LN == "Norwegian"){ FR_LN = "no" };
+            if(FR_LN == "Nyanja (Chichewa)"){ FR_LN = "ny" };
+            if(FR_LN == "Pashto"){ FR_LN = "ps" };
+            if(FR_LN == "Persian"){ FR_LN = "fa" };
+            if(FR_LN == "Polish"){ FR_LN = "pl" };
+            if(FR_LN == "Portuguese (Portugal, Brazil)"){ FR_LN = "pt" };
+            if(FR_LN == "Punjabi"){ FR_LN = "pa" };
+            if(FR_LN == "Romanian"){ FR_LN = "ro" };
+            if(FR_LN == "Russian"){ FR_LN = "ru" };
+            if(FR_LN == "Samoan"){ FR_LN = "sm" };
+            if(FR_LN == "Scots Gaelic"){ FR_LN = "gd" };
+            if(FR_LN == "Serbian"){ FR_LN = "sr" };
+            if(FR_LN == "Sesotho"){ FR_LN = "st" };
+            if(FR_LN == "Shona"){ FR_LN = "sn" };
+            if(FR_LN == "Sindhi"){ FR_LN = "sd" };
+            if(FR_LN == "Sinhala (Sinhalese)"){ FR_LN = "si" };
+            if(FR_LN == "Slovak"){ FR_LN = "sk" };
+            if(FR_LN == "Slovenian"){ FR_LN = "sl" };
+            if(FR_LN == "Somali"){ FR_LN = "so" };
+            if(FR_LN == "Spanish"){ FR_LN = "es" };
+            if(FR_LN == "Sundanese"){ FR_LN = "su" };
+            if(FR_LN == "Swahili"){ FR_LN = "sw" };
+            if(FR_LN == "Swedish"){ FR_LN = "sv" };
+            if(FR_LN == "Tagalog (Filipino)"){ FR_LN = "tl" };
+            if(FR_LN == "Tajik"){ FR_LN = "tg" };
+            if(FR_LN == "Tamil"){ FR_LN = "ta" };
+            if(FR_LN == "Telugu"){ FR_LN = "te" };
+            if(FR_LN == "Thai"){ FR_LN = "th" };
+            if(FR_LN == "Turkish"){ FR_LN = "tr" };
+            if(FR_LN == "Ukrainian"){ FR_LN = "uk" };
+            if(FR_LN == "Urdu"){ FR_LN = "ur" };
+            if(FR_LN == "Uzbek"){ FR_LN = "uz" };
+            if(FR_LN == "Vietnamese"){ FR_LN = "vi" };
+            if(FR_LN == "Welsh"){ FR_LN = "cy" };
+            if(FR_LN == "Xhosa"){ FR_LN = "xh" };
+            if(FR_LN == "Yiddish"){ FR_LN = "yi" };
+            if(FR_LN == "Yoruba"){ FR_LN = "yo" };
+            if(FR_LN == "Zulu"){ FR_LN = "zu" };
+           
+//////////////////////////////   TO_LN
+            if(TO_LN == "Afrikaans"){ TO_LN = "af" };
+            if(TO_LN == "Albanian "){ TO_LN = "sq" };
+            if(TO_LN == "Amharic"){ TO_LN = "am" };
+            if(TO_LN == "Arabic"){ TO_LN = "ar" };
+            if(TO_LN == "Armenian"){ TO_LN = "hy" };
+            if(TO_LN == "Azeerbaijani"){ TO_LN = "az" };
+            if(TO_LN == "Basque"){ TO_LN = "eu" };
+            if(TO_LN == "Belarusian"){ TO_LN = "be" };
+            if(TO_LN == "Bengali"){ TO_LN = "bn" };
+            if(TO_LN == "Bosnian"){ TO_LN = "bs" };
+            if(TO_LN == "Bulgarian"){ TO_LN = "bg" };
+            if(TO_LN == "Catalan"){ TO_LN = "ca" };
+            if(TO_LN == "Cebuano"){ TO_LN = "ceb" };
+            if(TO_LN == "Chinese (Simplified)"){ TO_LN = "zh-CN" };
+            if(TO_LN == "Chinese (Traditional)"){ TO_LN = "zh-TW" };
+            if(TO_LN == "Corsican"){ TO_LN = "co" };
+            if(TO_LN == "Croatian"){ TO_LN = "hr" };
+            if(TO_LN == "Czech"){ TO_LN = "cs" };
+            if(TO_LN == "Danish"){ TO_LN = "da" };
+            if(TO_LN == "Dutch"){ TO_LN = "nl" };
+            if(TO_LN == "English"){ TO_LN = "en" };
+            if(TO_LN == "Esperanto"){ TO_LN = "eo" };
+            if(TO_LN == "Estonian"){ TO_LN = "et" };
+            if(TO_LN == "Finnish"){ TO_LN = "fi" };
+            if(TO_LN == "French"){ TO_LN = "fr" };
+            if(TO_LN == "Frisian"){ TO_LN = "fy" };
+            if(TO_LN == "Galician"){ TO_LN = "gl" };
+            if(TO_LN == "Georgian"){ TO_LN = "ka" };
+            if(TO_LN == "German"){ TO_LN = "de" };
+            if(TO_LN == "Greek"){ TO_LN = "el" };
+            if(TO_LN == "Gujarati"){ TO_LN = "gu" };
+            if(TO_LN == "Haitian Creole"){ TO_LN = "ht" };
+            if(TO_LN == "Hausa"){ TO_LN = "ha" };
+            if(TO_LN == "Hawaiian"){ TO_LN = "haw" };
+            if(TO_LN == "Hebrew"){ TO_LN = "iw" };
+            if(TO_LN == "Hindi"){ TO_LN = "hi" };
+            if(TO_LN == "Hmong"){ TO_LN = "hmn" };
+            if(TO_LN == "Hungarian"){ TO_LN = "hu" };
+            if(TO_LN == "Icelandic"){ TO_LN = "is" };
+            if(TO_LN == "Igbo"){ TO_LN = "ig" };
+            if(TO_LN == "Indonesian"){ TO_LN = "id" };
+            if(TO_LN == "Irish"){ TO_LN = "ga" };
+            if(TO_LN == "Italian"){ TO_LN = "it" };
+            if(TO_LN == "Japanese"){ TO_LN = "ja" };
+            if(TO_LN == "Javanese"){ TO_LN = "jw" };
+            if(TO_LN == "Kannada"){ TO_LN = "kn" };
+            if(TO_LN == "Kazakh"){ TO_LN = "kk" };
+            if(TO_LN == "Khmer"){ TO_LN = "km" };
+            if(TO_LN == "Korean"){ TO_LN = "ko" };
+            if(TO_LN == "Kurdish"){ TO_LN = "ku" };
+            if(TO_LN == "Kyrgyz"){ TO_LN = "ky" };
+            if(TO_LN == "Lao"){ TO_LN = "lo" };
+            if(TO_LN == "Latin"){ TO_LN = "la" };
+            if(TO_LN == "Latvian"){ TO_LN = "lv" };
+            if(TO_LN == "Lithuanian"){ TO_LN = "lt" };
+            if(TO_LN == "Luxembourgish"){ TO_LN = "lb" };
+            if(TO_LN == "Macedonian"){ TO_LN = "mk" };
+            if(TO_LN == "Malagasy"){ TO_LN = "mg" };
+            if(TO_LN == "Malay"){ TO_LN = "ms" };
+            if(TO_LN == "Malayalam"){ TO_LN = "ml" };
+            if(TO_LN == "Maltese"){ TO_LN = "mt" };
+            if(TO_LN == "Maori"){ TO_LN = "mi" };
+            if(TO_LN == "Marathi"){ TO_LN = "mr" };
+            if(TO_LN == "Mongolian"){ TO_LN = "mn" };
+            if(TO_LN == "Myanmar (Burmese)"){ TO_LN = "my" };
+            if(TO_LN == "Nepali"){ TO_LN = "ne" };
+            if(TO_LN == "Norwegian"){ TO_LN = "no" };
+            if(TO_LN == "Nyanja (Chichewa)"){ TO_LN = "ny" };
+            if(TO_LN == "Pashto"){ TO_LN = "ps" };
+            if(TO_LN == "Persian"){ TO_LN = "fa" };
+            if(TO_LN == "Polish"){ TO_LN = "pl" };
+            if(TO_LN == "Portuguese (Portugal, Brazil)"){ TO_LN = "pt" };
+            if(TO_LN == "Punjabi"){ TO_LN = "pa" };
+            if(TO_LN == "Romanian"){ TO_LN = "ro" };
+            if(TO_LN == "Russian"){ TO_LN = "ru" };
+            if(TO_LN == "Samoan"){ TO_LN = "sm" };
+            if(TO_LN == "Scots Gaelic"){ TO_LN = "gd" };
+            if(TO_LN == "Serbian"){ TO_LN = "sr" };
+            if(TO_LN == "Sesotho"){ TO_LN = "st" };
+            if(TO_LN == "Shona"){ TO_LN = "sn" };
+            if(TO_LN == "Sindhi"){ TO_LN = "sd" };
+            if(TO_LN == "Sinhala (Sinhalese)"){ TO_LN = "si" };
+            if(TO_LN == "Slovak"){ TO_LN = "sk" };
+            if(TO_LN == "Slovenian"){ TO_LN = "sl" };
+            if(TO_LN == "Somali"){ TO_LN = "so" };
+            if(TO_LN == "Spanish"){ TO_LN = "es" };
+            if(TO_LN == "Sundanese"){ TO_LN = "su" };
+            if(TO_LN == "Swahili"){ TO_LN = "sw" };
+            if(TO_LN == "Swedish"){ TO_LN = "sv" };
+            if(TO_LN == "Tagalog (Filipino)"){ TO_LN = "tl" };
+            if(TO_LN == "Tajik"){ TO_LN = "tg" };
+            if(TO_LN == "Tamil"){ TO_LN = "ta" };
+            if(TO_LN == "Telugu"){ FR_LN = "te" };
+            if(TO_LN == "Thai"){ TO_LN = "th" };
+            if(TO_LN == "Turkish"){ TO_LN = "tr" };
+            if(TO_LN == "Ukrainian"){ TO_LN = "uk" };
+            if(TO_LN == "Urdu"){ TO_LN = "ur" };
+            if(TO_LN == "Uzbek"){ TO_LN = "uz" };
+            if(TO_LN == "Vietnamese"){ TO_LN = "vi" };
+            if(TO_LN == "Welsh"){ TO_LN = "cy" };
+            if(TO_LN == "Xhosa"){ TO_LN = "xh" };
+            if(TO_LN == "Yiddish"){ TO_LN = "yi" };
+            if(TO_LN == "Yoruba"){ TO_LN = "yo" };
+            if(TO_LN == "Zulu"){ TO_LN = "zu" };
+           
+
+           var socket = io.connect('http://luckykumardev1.herokuapp.com/');
+           
+            $('#poke').click(function () {
+                 socket.emit('message', { from_language: FR_LN, to_language: TO_LN, text: text});
+            })
+
+            socket.on('message', function(message) {
+                alert(message);
+            })
